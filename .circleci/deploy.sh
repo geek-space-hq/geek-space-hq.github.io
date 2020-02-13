85a5fadb416c66a1751a8e7ec9af518860152c2b
peacock <peacock0803sz@gmail.com>
2020-02-13 17:19:30 +0900

    :fire: Remove CircleCI files

--- .circleci/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-git config --global user.email $(git --no-pager show -s --format='%ae' HEAD)
-git config --global user.name $CIRCLE_USERNAME
-
-git add -f .
-git commit -m ":rocket: Deploy #$CIRCLE_BUILD_NUM from CircleCI [ci skip]" || true
-git push -f origin master
